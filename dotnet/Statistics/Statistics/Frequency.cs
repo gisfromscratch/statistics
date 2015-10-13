7cff0a6db0102cd8ee3d4019ec78dfce03e96e84
J.T. <gisfromscratch@live.de>
2015-10-13 02:53:41 +0200

    Added a simple dotnet statistics solution

---
+++ dotnet/Statistics/Statistics/Frequency.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Statistics
+{
+    /// <summary>
+    /// Represents the frequency statistics.
+    /// </summary>
+    internal class Frequency<TValue> where TValue : class
+    {
+        private readonly IDictionary<IComparable<TValue>, long> _frequencyValues;
+
+        private readonly IList<IComparable<TValue>> _modeValues;
+
+        private long _maxFrequency;
+
+        internal Frequency()
+        {
+            _frequencyValues = new Dictionary<IComparable<TValue>, long>();
+            _modeValues = new List<IComparable<TValue>>();
+        }
+
+        internal void AddValue(IComparable<TValue> value)
+        {
+            if (_frequencyValues.ContainsKey(value))
+            {
+                var frequency = ++_frequencyValues[value];
+                if (_maxFrequency == frequency)
+                {
+                    _modeValues.Add(value);
+                }
+                else if (_maxFrequency < frequency)
+                {
+                    _maxFrequency = frequency;
+                    _modeValues.Clear();
+                    _modeValues.Add(value);
+                }
+            }
+            else
+            {
+                _frequencyValues.Add(value, 1);
+                if (_maxFrequency == 1)
+                {
+                    _modeValues.Add(value);
+                }
+                else if (_maxFrequency < 1)
+                {
+                    _maxFrequency = 1;
+                    _modeValues.Add(value);
+                }
+            }
+        }
+
+        internal long GetFrequencyCount(IComparable<TValue> value)
+        {
+            if (_frequencyValues.ContainsKey(value))
+            {
+                return _frequencyValues[value];
+            }
+
+            return 0;
+        }
+
+        internal IList<IComparable<TValue>> ModeValues
+        {
+            get
+            {
+                return _modeValues;
+            }
+        }
+    }
+}
---
+++ dotnet/Statistics/Statistics/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Statistics
+{
+    class Program
+    {
+        static void Main(string[] arguments)
+        {
+            foreach (var argument in arguments) 
+            {
+                using (var reader = new StreamReader(File.OpenRead(argument)))
+                {
+                    Console.WriteLine(@"Reading {0}", argument);
+                    IList<string> fieldNames = new List<string>();
+                    IDictionary<string, Frequency<string>> frequencies = new Dictionary<string, Frequency<string>>();
+                    bool readHeader = true;
+                    string line;
+                    const long chunkSize = (long) 5e5;
+                    for (var lineNumber = 0; null != (line = reader.ReadLine()); lineNumber++)
+                    {
+                        var tokens = line.Split(',');
+                        var tokenCount = tokens.Length;
+                        for (var tokenIndex = 0; tokenIndex < tokenCount; tokenIndex++)
+                        {
+                            var nextToken = tokens[tokenIndex];
+                            if (readHeader)
+                            {
+                                fieldNames.Add(nextToken);
+                                frequencies.Add(nextToken, new Frequency<string>());
+                            }
+                            else
+                            {
+                                if (fieldNames.Count <= tokenIndex)
+                                {
+                                    break;
+                                }
+
+                                var fieldName = fieldNames[tokenIndex];
+                                if (frequencies.ContainsKey(fieldName))
+                                {
+                                    var frequency = frequencies[fieldName];
+                                    frequency.AddValue(nextToken);
+                                }
+                            }
+                        }
+                        if (readHeader)
+                        {
+                            readHeader = !fieldNames.Any();
+                        }
+                        if (0 != lineNumber && 0 == (lineNumber % chunkSize))
+                        {
+                            Console.WriteLine(@"{0} lines read", lineNumber);
+                        }
+                    }
+
+                    Console.WriteLine();
+                    foreach (var fieldEntry in frequencies) 
+                    {
+                        var fieldName = fieldEntry.Key;
+                        var frequency = fieldEntry.Value;
+                        Console.WriteLine("{0}\t", fieldName);
+                        var modeValues = frequency.ModeValues;
+                        foreach (var modeValue in modeValues)
+                        {
+                            Console.WriteLine("\t{0}:\t{1}", modeValue, frequency.GetFrequencyCount(modeValue));
+                        }
+                    }
+                    Console.WriteLine();
+                }
+            }
+        }
+    }
+}
